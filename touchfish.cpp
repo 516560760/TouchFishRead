fa3b54730a258c5cca5781bd2e9d592b95ebb4fc
guoxu <guoxu@topjoy.com>
2022-02-28 18:24:25 +0800

    find and jump

--- touchfish.cpp
+++ touchfish.cpp
@@ -7,6 +7,7 @@ extern "C"
 	#include <lua.h>
 	#include <lualib.h>
 	#include <lauxlib.h>
+	#include <corecrt_memory.h>
 }
 
 void set_global_string(lua_State* L, const char* k, const char* v)
@@ -57,21 +58,35 @@ int main(int argc, char* argv[])
 	int ret = luaL_dofile(L, "script");
 	if (ret)
 	{
-		printf("load script failed, check syntax error please.");
+		printf("load main.lua failed");
 		return EXIT_SUCCESS;
 	}
 	call_global_commond(L, "LOAD_FILE", argv[1]);
 
 	char cmd = 0;
 	char lua_cmd[2]{0, '\0'};
-	while ((cmd = getch()) != 'q')
+	char i_cmd[256];
+	memset(i_cmd, '\0', 256);
+	while (((cmd = getch()) != 'q') && (cmd != 'Q'))
 	{
-		lua_cmd[0] = cmd;
-		int ret = call_global_commond(L, "CHECK_CMD", lua_cmd);
-		if (!ret)
+		int ret = 1;
+		if (cmd == 'i' || cmd == 'I')
 		{
 			clear_screen();
-			call_global_commond(L, "EXEC_CMD", lua_cmd);
+			printf("input commond and params.\n");
+			gets_s(i_cmd);
+			clear_screen();
+			call_global_commond(L, "EXEC_SCMD", i_cmd);
+		}
+		else
+		{
+			lua_cmd[0] = cmd;
+			ret = call_global_commond(L, "CHECK_CMD", lua_cmd);
+			if (!ret)
+			{
+				clear_screen();
+				call_global_commond(L, "EXEC_CMD", lua_cmd);
+			}
 		}
 	}
 	call_global_commond(L, "EXIT", "");
